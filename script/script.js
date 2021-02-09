c513126055aca768e1d885b602f12dd6307743fb
fohlle <patte2000@hotmail.com>
2021-02-09 20:06:06 +0100

    update board

---
+++ script/script.js
@@ -0,0 +1,7 @@
+
+let game = new TicTacToe();
+let anna = new Player("anna","X")
+let emma = new Player("emma","O")
+
+game.play();
+game.make_click();
--- script/tictactoe.js
+++ script/tictactoe.js
@@ -3,6 +3,41 @@
 class TicTacToe {
 
   constructor(){
-    
+    this.board = this.make_board();
+    this.add_reset();
+    this.player_one = new Player("me","X")
+    this.player_two = new Player("me","O")
+  }
+
+  make_click(){
+    let child = [...document.querySelectorAll(".child")]
+    child.forEach( (n) => {
+      n.addEventListener("click", (e) => {
+        e.target.innerText = this.board[e.target.id-1]
+      })
+    })
+  }
+
+  make_board(){
+    return this.board = [11,"two",3,4,5,6,7,8,9]
+  }
+
+  update_board(player){
+    this.board[0] = player.type
+  }
+
+  add_reset(){
+    let button = document.querySelector("button")
+    let child = [...document.querySelectorAll(".child")]
+    button.addEventListener("click",() => {
+      child.forEach( (n) => {
+        n.innerText = ""
+      })
+    })
+  }
+
+  play(){
+    this.update_board(this.player_one)
+    this.update_board(this.player_two)
   }
 }
