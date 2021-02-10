358510f753ce5fd4354968d0f4436df4c067e4cb
fohlle <patte2000@hotmail.com>
2021-02-10 08:52:22 +0100

    logic done

--- script/script.js
+++ script/script.js
@@ -3,5 +3,4 @@ let game = new TicTacToe();
 let anna = new Player("anna","X")
 let emma = new Player("emma","O")
 
-game.play();
 game.make_click();
--- script/tictactoe.js
+++ script/tictactoe.js
@@ -5,25 +5,41 @@ class TicTacToe {
   constructor(){
     this.board = this.make_board();
     this.add_reset();
-    this.player_one = new Player("me","X")
-    this.player_two = new Player("me","O")
+    this.player_one = new Player("one","X")
+    this.player_two = new Player("two","O")
+    this.counter = 0;
+    this.win_pos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]];
+    this.win = false;
+    this.win_array = [];
   }
 
   make_click(){
     let child = [...document.querySelectorAll(".child")]
     child.forEach( (n) => {
       n.addEventListener("click", (e) => {
+
+        if(this.board[e.target.id - 1] === 1){
+        this.update_board(e.target.id);
         e.target.innerText = this.board[e.target.id-1]
+        this.test_winner();
+        }
+
       })
     })
   }
 
   make_board(){
-    return this.board = [11,"two",3,4,5,6,7,8,9]
+    return this.board = [1,1,1,1,1,1,1,1,1]
   }
 
-  update_board(player){
-    this.board[0] = player.type
+  update_board(target){
+    if(this.counter % 2 == 0){
+      this.board[target-1] = "X"
+    }else {
+      this.board[target-1] = "O"
+    }
+    console.log(this.counter)
+    this.counter++;
   }
 
   add_reset(){
@@ -32,12 +48,52 @@ class TicTacToe {
     button.addEventListener("click",() => {
       child.forEach( (n) => {
         n.innerText = ""
+        this.win_array = [];
+        this.board = [1,1,1,1,1,1,1,1,1]
+        this.counter = 0;
       })
     })
   }
 
-  play(){
-    this.update_board(this.player_one)
-    this.update_board(this.player_two)
+  test_winner(){
+    this.win_pos.forEach( (arr) => {
+      arr.forEach( (n) => {
+        this.win_array.push(this.board[n])
+      })
+      if(this.win_array.every( (n) => n == "X")){
+        this.win = "X"
+      }
+      if(this.win_array.every( (n) => n == "O")){
+        this.win = "O"
+      }
+      this.win_array = [];
+    })
+    this.game_over();
   }
+
+  reset(){
+    let child = [...document.querySelectorAll(".child")]
+      child.forEach( (n) => {
+        n.innerText = ""
+        this.win = false;
+        this.win_array = [];
+        this.board = [1,1,1,1,1,1,1,1,1]
+        this.counter = 0;
+      })
+  }
+
+  game_over(){
+    if(this.win !== false){
+
+      setTimeout( (n) => {
+        alert(`Winner is ${this.win}`)
+      this.reset();
+      return true;
+      },500)
+
+    }
+    return false;
+  }
+
+  
 }
